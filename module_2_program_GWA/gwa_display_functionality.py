788c6d32061ba5cc7e6f045b492482542da59d2b
Tres <encantotresinigo@gmail.com>
2026-04-30 23:05:08 +0800

    Created a try statement to handle the program's function. Used a stip function to remove the spaces so that the format would be 'Student, GWA'. Used if statements to find the current student with the highest GWA.

--- module_2_program_GWA/gwa_display_functionality.py
+++ module_2_program_GWA/gwa_display_functionality.py
@@ -9,3 +9,21 @@ class GwaSorter:
 
     def highest_gwa(self):
         highest_student = None
+
+        try:
+            with open(self.filename, 'r') as file:
+                for line in file:
+                    #The format is: Name, GWA
+                    if line.strip():
+                        parts = line.strip().split(',')
+                        if len(parts) == 2:
+                            name = parts[0].strip()
+                            gwa = parts[1].strip()
+
+                            current_student = StudentName(name, gwa)
+
+                            if highest_student is None or current_student.gwa < highest_student.gwa:
+                                highest_student = current_student
+            
+            return highest_student
+
