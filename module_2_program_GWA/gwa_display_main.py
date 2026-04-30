6d17dde01d61bde441410e44c9b650be75e38f6c
Tres <encantotresinigo@gmail.com>
2026-04-30 23:14:00 +0800

    Imported the functionality and the class from the functionality file and used the GWA sorter function to sort and show the student with the highest GWA among the class.

--- module_2_program_GWA/gwa_display_main.py
+++ module_2_program_GWA/gwa_display_main.py
@@ -1,1 +1,12 @@
-print("GWA Display")
+from gwa_display_functionality import GwaSorter
+
+def main_program():
+    print("--- Top Student of the Class ---")
+    sorter = GwaSorter("students.txt")
+    top_student = sorter.highest_gwa()
+
+    if top_student:
+        print(f"The top student of the class is {top_student.name} with GWA of {top_student.gwa}")
+
+if __name__ == "__main__":
+    main_program()
