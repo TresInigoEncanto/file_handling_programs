517759e4c8fda8dca07c91cc08cb52225d47830b
Tres <encantotresinigo@gmail.com>
2026-04-30 22:42:45 +0800

    Created two classes, one for the student's name, the other class is for the GWA sorter.

--- module_2_program_GWA/gwa_display_functionality.py
+++ module_2_program_GWA/gwa_display_functionality.py
@@ -1,1 +1,11 @@
-print("GWA Functionality")
+class StudentName:
+    def __init__(self, name, gwa):
+        self.name = name
+        self.gwa = float(gwa)
+
+class GwaSorter:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def highest_gwa(self):
+        highest_student = None
