4a955ee2adb769ad7053c56794aed6550e1086f9
Tres <encantotresinigo@gmail.com>
2026-05-04 01:17:50 +0800

    Added a with statement and used a for append to avoid overwriting with the previous log sessions. Used a strftime to return the string representation of the current date and time during the log session.

--- module_2_program_multiple_line_of_text/diary_multiple_lines.py
+++ module_2_program_multiple_line_of_text/diary_multiple_lines.py
@@ -1,1 +1,11 @@
 import datetime
+
+class MyDiary:
+    def __init__(self, filename="mylife.txt"):
+        self.filename = filename
+
+    def write_entry(self):
+        """Multi-line input loop as diary."""
+        try:
+            with open(self.filename, "a") as file:
+                print(f"--- My Log Session: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} ---")
