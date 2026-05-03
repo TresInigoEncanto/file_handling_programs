b21164e68dd1092dc0eb448e73f8ca2ddf4afcd8
Tres <encantotresinigo@gmail.com>
2026-05-04 01:30:26 +0800

    Added a while loop for the input in the diary. Used an if statement for the y/n choice. Used an input output error handling for reading and writing the diary file.

--- module_2_program_multiple_line_of_text/diary_multiple_lines.py
+++ module_2_program_multiple_line_of_text/diary_multiple_lines.py
@@ -9,3 +9,16 @@ class MyDiary:
         try:
             with open(self.filename, "a") as file:
                 print(f"--- My Log Session: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} ---")
+
+                while True:
+                    entry_line = input("Log your line: ")
+                    file.write(entry_line + "\n")
+
+                    choices = input("Are there more lines? y/n ").lower()
+                    if choices != 'y':
+                        print("My entry saved successfully.")
+                        break
+        except IOError as error:
+            print(f"An error occured while writing the file: {error}")
+
+
