c2dd28be9798d44489573b515330ce3a3874b719
Tres <encantotresinigo@gmail.com>
2026-05-04 02:15:41 +0800

    Created an integer sorter object to create the separate text files for where the odd and the even numbers will head onto after being read from inside the main text file.

--- module_2_program_two_separate_text_files/separate_text_files_program.py
+++ module_2_program_two_separate_text_files/separate_text_files_program.py
@@ -1,1 +1,9 @@
 import math
+
+class IntegerSorter:
+    def __init__(self, source_file="integers.txt"):
+        self.source_file = source_file
+        self.odd_file = "triple.txt"
+        self.even_file = "double.txt"
+    
+   
